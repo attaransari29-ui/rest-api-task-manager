dace1624fdceea692e16258a31fa3c248ace071e
Attar Ansari <attaransari29@gmail.com>
2026-02-22 16:11:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify, render_template
-from flask_mysqldb import MySQL
+import pymysql
+pymysql.install_as_MySQLdb()
+
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import (
     JWTManager,
